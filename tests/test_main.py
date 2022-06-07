1179ba5dd2fc321dd15526a343b5f7a820fc3dfd
Nick DeRobertis <whoopnip@gmail.com>
2022-06-07 19:07:19 -0400

    chore: Restructure test in preparation for more similar tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,8 @@
+from typing import Optional
+
 from tests.config import FILE_TREE_ONE, FILE_TREE_TWO
 from treecomp import diff_file_trees
+from treecomp.main import FileDiffWithDirs
 
 
 def test_diff_file_trees():
@@ -10,26 +13,38 @@ def test_diff_file_trees():
     assert comp.dir2 == FILE_TREE_TWO
 
     a_diff = comp.diff_for("a.txt")
-    assert a_diff is not None
-    assert a_diff.diff.exists_in_dir1 is True
-    assert a_diff.diff.exists_in_dir2 is True
-    assert "@@ -1,2 +1,2 @@\n A\n-one\n+two" in a_diff.diff.line_diff
-    assert "a.txt" in a_diff.diff.line_diff
+    _assert_a_diff_between_one_and_two_is_correct(a_diff)
 
     b_diff = comp.diff_for("b.txt")
-    assert b_diff is not None
-    assert b_diff.diff.exists_in_dir1 is True
-    assert b_diff.diff.exists_in_dir2 is False
-    assert "@@ -0,0 +1,2 @@\n+B\n+one" in b_diff.diff.line_diff
-    assert "b.txt" in b_diff.diff.line_diff
+    _assert_b_diff_between_one_and_two_is_correct(b_diff)
 
     c_diff = comp.diff_for("c.txt")
-    assert c_diff is not None
-    assert c_diff.diff.exists_in_dir1 is False
-    assert c_diff.diff.exists_in_dir2 is True
-    assert "@@ -0,0 +1,2 @@\n+C\n+two" in c_diff.diff.line_diff
-    assert "c.txt" in c_diff.diff.line_diff
+    _assert_c_diff_between_one_and_two_is_correct(c_diff)
 
     assert a_diff.diff.line_diff in str(comp)
     assert b_diff.diff.line_diff in str(comp)
     assert c_diff.diff.line_diff in str(comp)
+
+
+def _assert_a_diff_between_one_and_two_is_correct(diff: Optional[FileDiffWithDirs]):
+    assert diff is not None
+    assert diff.diff.exists_in_dir1 is True
+    assert diff.diff.exists_in_dir2 is True
+    assert "@@ -1,2 +1,2 @@\n A\n-one\n+two" in diff.diff.line_diff
+    assert "a.txt" in diff.diff.line_diff
+
+
+def _assert_b_diff_between_one_and_two_is_correct(diff: Optional[FileDiffWithDirs]):
+    assert diff is not None
+    assert diff.diff.exists_in_dir1 is True
+    assert diff.diff.exists_in_dir2 is False
+    assert "@@ -0,0 +1,2 @@\n+B\n+one" in diff.diff.line_diff
+    assert "b.txt" in diff.diff.line_diff
+
+
+def _assert_c_diff_between_one_and_two_is_correct(diff: Optional[FileDiffWithDirs]):
+    assert diff is not None
+    assert diff.diff.exists_in_dir1 is False
+    assert diff.diff.exists_in_dir2 is True
+    assert "@@ -0,0 +1,2 @@\n+C\n+two" in diff.diff.line_diff
+    assert "c.txt" in diff.diff.line_diff
